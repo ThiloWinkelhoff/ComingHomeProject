bd2db67d61fe862ce23fe2f9fdd0922092d752ed
Thilo Winkelhoff <t.winkelhoff@polipol.de>
2025-03-28 22:29:49 +0100

    Refactor code using SRP

--- Infrastructure/Configuration/ConfigLEDmatrix.py
+++ Infrastructure/Configuration/ConfigLEDmatrix.py
@@ -8,24 +8,30 @@ from luma.core.virtual import viewport
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
-def configure():
+def InitializeMatrix():
     serial = spi(port=0, device=0, gpio=noop())
-    device = max7219(serial, cascaded=n, block_orientation=block_orientation, 
+    return max7219(serial, cascaded=n, block_orientation=block_orientation, 
                      rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
-    print("Created device")
 
-def printMessage(msg: str):
-    configure()
-    show_message(device, msg, fill="white", font=proportional(CP437_FONT))
+def display_text(device, msg, font=CP437_FONT):
+    print(msg)
+    show_message(device, msg, fill="white", font=proportional(font))
     time.sleep(1)
 
-def printVerticalScorllingMsg(words):
-    configure()
-    virtual = viewport(device, width=device.width, height=len(words))
+def scroll_text(device, msg, font=LCD_FONT, speed=0.1):
+    print(msg)
+    show_message(device, msg, fill="white", font=proportional(font), scroll_delay=speed),
+
+def vertical_scroll(device, words):
+    print("Vertical scrolling")
+    virtual = viewport(device, width=device.width, height=len(words) * 8)
     with canvas(virtual) as draw:
         for i, words in enumerate(words):
             text(draw, (0, i * 8), word, fill="white", font=proportional(CP437_FONT))
 
     for i in range(virtual.height - device.height):
         virtual.set_position((0,i))
-        time.sleep(0.1)
+        time.sleep(0.05)
+
+if __name__ == "__main__":
+    parser= arg
