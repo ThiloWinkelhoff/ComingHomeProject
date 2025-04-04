784b52539eed15b1148497da61c819331d584573
Thilo Winkelhoff <t.winkelhoff@polipol.de>
2025-04-04 12:13:27 +0200

    Refactor ConfigRouter

--- Core/src/ConfigRouter.py
+++ Core/src/ConfigRouter.py
@@ -1,17 +1,13 @@
 import json
 from fritzconnection.lib.fritzhosts import FritzHosts
 
-# Load secrets from the JSON file
-with open('secrets.json') as f:
-    secrets = json.load(f)
+def configure_Router():
+    with open('secrets.json') as f:
+        secrets = json.load(f)
+    fritz_host = secrets['fritz_host']
+    username = secrets['username']
+    password = secrets['password']
+    return FritzHosts(address=fritz_host, user=username, password=password)
 
-fritz_host = secrets['fritz_host']
-username = secrets['username']
-password = secrets['password']
-
-fh = FritzHosts(address=fritz_host, user=username, password=password)
-
-connected_devices = fh.get_hosts_info()
-
-for device in connected_devices:
-    print(f"Name: {device['name']}, IP: {device['ip']}, MAC: {device['mac']}, Status: {device['status']}")
+def get_session_devices(fh: FritzHosts):
+    return fh.get_active_hosts()
