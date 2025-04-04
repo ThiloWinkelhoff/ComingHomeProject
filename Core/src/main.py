0057f07d06e013e7ab84caea0b663ca93beb856a
Thilo Winkelhoff <t.winkelhoff@polipol.de>
2025-04-04 12:13:45 +0200

    Create main script

---
+++ Core/src/main.py
@@ -0,0 +1,18 @@
+import time
+import device_monitor
+import ConfigRouter
+
+if __name__ == "__main__":
+    known_devices = ""
+    fh = ConfigRouter.configure_Router()
+    session_devices = ConfigRouter.get_session_devices(fh)
+
+    while True:
+        connectedDevices = ConfigRouter.get_session_devices(fh)
+        for connectedDevice in connectedDevices:
+            if not(session_devices.__contains__(connectedDevice)):
+                if known_devices.__contains__(connectDevice):
+                    #invoke ClientArrivedEvent()
+                    print("invoke ClientArrivedEvent")
+                session_devices.__add__(connectedDevice)
+        time.sleep(10000)
