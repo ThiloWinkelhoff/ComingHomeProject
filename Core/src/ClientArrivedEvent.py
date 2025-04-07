362ac471282055a65d099a3cd837568ac54b8670
Thilo Winkelhoff <t.winkelhoff@polipol.de>
2025-04-07 19:03:04 +0200

    Move device init into main

--- Core/src/ClientArrivedEvent.py
+++ Core/src/ClientArrivedEvent.py
@@ -1,7 +1,6 @@
 from typing import List
 import ConfigLEDmatrix
 
-def handle_client_arrival(scripts: List[str]):
-    device = ConfigLEDmatrix.initialize_device()
+def handle_client_arrival(matrix_device, scripts: List[str]):
     for script in scripts:
-        ConfigLEDmatrix.vertical_scroll(device, script)
+        ConfigLEDmatrix.vertical_scroll(matrix_device, script)
--- Core/src/main.py
+++ Core/src/main.py
@@ -1,6 +1,7 @@
 import time
 import ClientArrivedEvent
 import ConfigRouter
+import ConfigLEDmatrix
 from typing import List
 
 class Device:
@@ -12,14 +13,15 @@ if __name__ == "__main__":
     known_devices = "" ;#hier müssen die devices aus der json rein
     fh = ConfigRouter.configure_Router()
     session_devices = ConfigRouter.get_session_devices(fh)
-
+    matrix_device = ConfigLEDmatrix.initialize_device()
+    
     while True:
         connectedDevices = ConfigRouter.get_session_devices(fh)
         for connectedDevice in connectedDevices:
             if not(session_devices.__contains__(connectedDevice)):
                 if known_devices.__contains__(connectedDevice):
                   #  var temp = known_devices.Where(x => x.IP == connectedDevice)
-                    ClientArrivedEvent.handle_client_arrival() #hier müssen die scripte von dem device ausgegeben werden
+                    ClientArrivedEvent.handle_client_arrival(matrix_device) #hier müssen die scripte von dem device ausgegeben werden
                     print("invoke ClientArrivedEvent")
                 session_devices.__add__(connectedDevice)
         time.sleep(10000)
