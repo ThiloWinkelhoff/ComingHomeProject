83e4bebda4e2d2ec6cc22bc3bf105478efa06c55
Lukas Labudde <LukasLabudde1@gmail.com>
2025-04-11 12:22:31 +0200

    Updated main

--- Core/src/main.py
+++ Core/src/main.py
@@ -19,13 +19,7 @@ if __name__ == "__main__":
 
         # Update device status (you might want to check changes, new, or disconnected devices)
         new_devices, disconnected_devices, connected_devices = device_monitor.detect_changes()
-
-        print("NEW DEVICES:")
-        print(new_devices)
-        print("DISCONNECTED DEVICES:")
-        print(disconnected_devices)
-        print("CONNECTED DEVICES:")
-        print(connected_devices)        
+ 
         # Collect unique scripts from the connected devices
         unique_scripts = list({script for device in connected_devices for script in device.scripts})
 
