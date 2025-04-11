6cc1752131af2ccc85498f35b5a18affe8ec48fc
Thilo Winkelhoff <t.winkelhoff@polipol.de>
2025-04-11 11:54:06 +0200

    Fix syntax error

--- Core/src/ConfigLEDmatrix.py
+++ Core/src/ConfigLEDmatrix.py
@@ -9,9 +9,9 @@ def initialize_device(n = 1, block_orientation = 0, rotate = 0, inreverse = Fals
                      rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
 
 if __name__ == "__main__":
-    parser= argparse.ArgumentParser(description='matrix_configuration arguments')
+    parser = argparse.ArgumentParser(description='matrix_configuration arguments')
     parser.add_argument('--cascaded', '-n', type=int, default=1, help='Number of cascaded MAX7219 LED matrices')
-    parser.add_argument('--block-orientation', type=int, default=0, choices=[0,90,-90] help='Corrects block orientation when wired vertically')
-    parser.add_argument('--rotate', type=int, default=0, choices=[0,1,2,3], help='Rotate display 0=0°, 1=90°, 2=180°, 3=270°')
+    parser.add_argument('--block-orientation', type=int, default=0, choices=[0, 90, -90], help='Corrects block orientation when wired vertically')
+    parser.add_argument('--rotate', type=int, default=0, choices=[0, 1, 2, 3], help='Rotate display 0=0°, 1=90°, 2=180°, 3=270°')
 
     args = parser.parse_args()
