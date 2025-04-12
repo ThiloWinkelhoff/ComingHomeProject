a7ae21512474a94c38ea081241dbc322d1f7b19b
Thilo Winkelhoff <t.winkelhoff@polipol.de>
2025-04-12 22:06:28 +0200

    Update ClientArrivedEvent.py

--- Core/src/ClientArrivedEvent.py
+++ Core/src/ClientArrivedEvent.py
@@ -2,4 +2,4 @@ from typing import List
 import led_matrix_display as display
 def handle_client_arrival(matrix_device, scripts: List[str]):
     for script in scripts:
-        display.vertical_scroll(matrix_device, script)
+        display.display_text(matrix_device, script)
