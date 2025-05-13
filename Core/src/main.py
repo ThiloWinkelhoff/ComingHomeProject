4c4259bd02d33cc407dcec054c0117a9629329fc
Thilo Winkelhoff <t.winkelhoff@polipol.de>
2025-05-13 19:01:40 +0200

    Enable matrix

--- Core/src/main.py
+++ Core/src/main.py
@@ -7,15 +7,13 @@ from typing import List
 
 if __name__ == "__main__":
 
-    esp_ip = "http://192.168.1.42"
-    msg = {"msg": "Hallo aus dem Pi!"}
     # Initial detection of devices
     new_devices, disconnected_devices, connected_devices = device_monitor.detect_changes()
 
     # Set to collect unique scripts from connected devices
     unique_scripts = list({script for device in connected_devices for script in device.scripts})
 
-    #matrix_device = ConfigLEDmatrix.initialize_device(1, 0, 0, False)
+    matrix_device = ConfigLEDmatrix.initialize_device(1, 0, 0, False)
     
     while True:
         print("Running...")
@@ -33,13 +31,12 @@ if __name__ == "__main__":
         if not unique_scripts:
                 current_time = time.strftime("%H:%M:%S")
 
-                print(f"Current Time: {current_time}")
-                #matrix.display_text(matrix_device, current_time)
+                #print(f"Current Time: {current_time}")
+                matrix.display_text(matrix_device, current_time)
         else:
-            res = requests.post(f"{esp_ip}/send", data=msg)
             for script in unique_scripts:
-                print(script)
-                # matrix.display_text(matrix_device, script)
+                #print(script)
+                matrix.display_text(matrix_device, script)
 
         # Pause before the next loop
         time.sleep(1)
